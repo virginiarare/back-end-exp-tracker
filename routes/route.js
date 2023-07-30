8aff49121bf94ed5ec5603744d25fe5bc999ff21
Virginia Ramos <virginia.rare@gmail.com>
2023-07-30 02:22:12 -0500

    Chaging routes

--- routes/route.js
+++ routes/route.js
@@ -1,16 +1,16 @@
 const routes = require('express').Router();
 const controller = require('../controller.js');
 
-routes.route('/api/categories')
+routes.route('/categories')
     .post(controller.create_Categories)
     .get(controller.get_Categories)
 
-routes.route('/api/transaction')
+routes.route('/transaction')
     .post(controller.create_Transaction)
     .get(controller.get_Transaction)
     .delete(controller.delete_Transaction)
 
-routes.route('/api/labels')
+routes.route('/labels')
     .get(controller.get_Labels)
 
 module.exports = routes;
